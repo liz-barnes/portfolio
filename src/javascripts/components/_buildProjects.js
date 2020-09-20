61a9f16586cb00096d787f3e5f92cfd214b675a4
Liz <liz@Liz-s-Mac.local>
2020-09-20 10:30:18 -0500

    create function to print project cards

--- src/javascripts/components/_buildProjects.js
+++ src/javascripts/components/_buildProjects.js
@@ -1,16 +1,12 @@
-import projectData from '../helpers/data/_projectsData';
-
 const buildProjects = (array) => {
   array.forEach((item) => {
     $('#projectsContainer').append(
       `<div id="project-${item.id}">
         <div id="imageContainer">
           <img src="${item.screenshot}" id="projectImage">
-        </div>
-        
-
-
-    
-    `);
+        </div>`
+    );
   });
 };
+
+export default { buildProjects };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,12 @@
 import navbarEvents from './components/_navigation';
+import projectData from './helpers/data/_projectsData';
+import buildProjects from './components/_buildProjects';
 
 import '../styles/main.scss';
 
 const init = () => {
   navbarEvents.navbarEvents();
+  buildProjects.buildProjects(projectData.getProjects());
 };
 
 init();
