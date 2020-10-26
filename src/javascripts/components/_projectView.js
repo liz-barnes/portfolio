20abc76ba9959124636a416825383603d49279bc
Liz <liz@Liz-s-Mac.local>
2020-10-26 15:30:25 -0500

    made changes to project cards size and removed the background with box shadow

--- src/javascripts/components/_projectView.js
+++ src/javascripts/components/_projectView.js
@@ -6,7 +6,9 @@ const projectView = (projectObject) => {
         <h3 id="project-title">${projectObject.title}</h3>
         <h5>${projectObject.type}</h5>
         <h6>demo</h6>
-        <div id="project-demo"></div>
+        <div id="project-demo">
+          <img src="https://recordit.co/6JB7D7vaDc.gif" alt="Project demo gif">
+        </div>
         <p id="project-description">${projectObject.description}</p>
         <h6>role</h6>
         <p id="project-role">${projectObject.role}</p>
