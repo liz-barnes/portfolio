0665e5ed6b3fdb88eefdafed4f6fbe6926c1de78
Liz <liz@Liz-s-Mac.local>
2020-10-25 22:16:17 -0500

    style projects view page

--- src/javascripts/components/_projectView.js
+++ src/javascripts/components/_projectView.js
@@ -1,24 +1,36 @@
-import projectData from '../helpers/data/_projectsData';
+// import projectData from '../helpers/data/_projectsData';
 
-const projectView = (projectId) => {
-  projectData.getProjects().forEach((project) => {
-    if (project.id === '0') {
-      console.warn(projectId);
-      console.warn(project.id);
-      $('#app').html(`
+const projectView = (projectObject) => {
+  $('#app').html(`
       <div id=project-view-container>
-        <h3>${project.title}</h3>
-        <h5>${project.type}</h5>
+        <h3 id="project-title">${projectObject.title}</h3>
+        <h5>${projectObject.type}</h5>
         <h6>demo</h6>
         <div id="project-demo"></div>
-        <p id="project-description">${project.description}</p>
+        <p id="project-description">${projectObject.description}</p>
         <h6>role</h6>
-        <p id="project-role">${project.role}</p>
+        <p id="project-role">${projectObject.role}</p>
         <h6>tools</h6>
-        <p id="project-tools">${project.technologiesUsed}</p>
+        <p id="project-tools">${projectObject.technologiesUsed}</p>
         <h6>demo</h6>
       </div>`);
-    }
-  });
+  // projectData.getProjects().find(obj => obj.id === projectId);
+  // console.warn(projectObject);
+  // console.warn(projectObject.id);
+  // if (projectObject === '5') {
+  //   $('#app').html(`
+  //     <div id=project-view-container>
+  //       <h3>${projectObject.title}</h3>
+  //       <h5>${projectObject.type}</h5>
+  //       <h6>demo</h6>
+  //       <div id="project-demo"></div>
+  //       <p id="project-description">${projectObject.description}</p>
+  //       <h6>role</h6>
+  //       <p id="project-role">${projectObject.role}</p>
+  //       <h6>tools</h6>
+  //       <p id="project-tools">${projectObject.technologiesUsed}</p>
+  //       <h6>demo</h6>
+  //     </div>`);
+  // }
 };
 export default { projectView };
