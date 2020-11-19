dd9e0c5c31e0f3b60bf3d6a48bfffb3f933b35d8
Liz <liz@Liz-s-Mac.local>
2020-11-19 13:45:09 -0600

    style projects page

--- src/javascripts/components/_buildProjects.js
+++ src/javascripts/components/_buildProjects.js
@@ -8,12 +8,10 @@ const buildProjects = () => {
         $('#projectsContainer').append(`
         <div id="project-${project.firebaseKey}" class="project-card-container" style="background-image: url(${project.screenshot})">
           <div class="project-card">
-          <img class="project-img" src="${project.screenshot}">
             <div class="hover-content">
               <p class="project-type">${project.type}</p>
               <h6 class="project-title">${project.title}</h6>
-              <div class="project-btn-container">
-                <button class="outline view-project-btn project-btn" id="${project.firebaseKey}">view project</button>
+              <div class="project-btn-container mt-4">
                 <a href="${project.url}" target="_blank" class="outline project-btn visit-site-btn" id="${project.firebaseKey}">visit site</a>
               </div>
               </div>
@@ -29,5 +27,8 @@ const buildProjects = () => {
     });
   });
 };
+
+// <button class="btn-outline view-project-btn project-btn" id="${project.firebaseKey}">view project</button>
+// <a href="${project.url}" target="_blank" class="outline project-btn visit-site-btn" id="${project.firebaseKey}">visit site</a>
 
 export default { buildProjects };
